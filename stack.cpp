1bde00c0b2bdc1464454cdb702a958c4e6d50e83
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-09-23 17:12:09 +0300

    Added resize_stack function

--- stack.cpp
+++ stack.cpp
@@ -36,23 +36,11 @@ error_type stack_dtor(Stack* stk) {
 
 error_type stack_push(Stack* stk, Elem_t value) {
     if(stk->is_exist == 0) return stack_is_not_exist;
-    if(stk->size == stk->capacity - 1) {
-        stk->data = (Elem_t*)realloc(stk->data, stk->capacity * 2 * sizeof(Elem_t));
-        for(int i = 0; i < stk->size + 2; i++)
-            stk->data[stk->size  + i] = void_elem;
-        if(stk->data == NULL)
-            return mem_alloc_err;
-        stk->capacity *= 2;
-        stk->data[stk->size] = value;
-        stk->size += 1;
+    stack_resize_capacity(stk);
+    stk->data[stk->size] = value;
+    stk->size += 1;
 
-        return no_err;
-    } else {
-        stk->data[stk->size] = value;
-        stk->size++;
-
-        return no_err;
-    }
+    return no_err;
 }
 
 error_type stack_pop(Stack* stk, Elem_t* poped) {
@@ -60,21 +48,16 @@ error_type stack_pop(Stack* stk, Elem_t* poped) {
     if(stk->is_exist == 0) return stack_is_not_exist;
     if(stk->size == 0)
         return out_of_index_err;
-    if(stk->size-- == stk->capacity / 2 && stk->capacity / 2 >= min_capacity) {
-        stk->data = (Elem_t*)realloc(stk->data, stk -> capacity  / 2 * sizeof(Elem_t));
-        stk->capacity = stk->capacity /  2;
-        if(stk->data == NULL)
-            return mem_alloc_err;
-        *poped = stk->data[(stk->size)];
-        stk->data[(stk->size)] = void_elem;
-    } else {
-        *poped = stk->data[(stk->size)];
-        stk->data[(stk->size)] = void_elem;
+    stk->size--;
+    *poped = stk->data[(stk->size)];
+    stk->data[(stk->size)] = void_elem;
+    if(stack_resize_capacity(stk) == no_err) {
+        return no_err;
     }
-    return no_err;
+    return critical_err;
 }
 
-error_type dump(Stack* stk, const char* file, const char* func, const int line) {
+error_type stack_dump(Stack* stk, const char* file, const char* func, const int line) {
     printf("Dump called in file - %s function  - %s on line - %d", file, func, line);
     if(stk->is_exist == 0) {
         return stack_is_not_exist;
@@ -114,6 +97,13 @@ error_type get_error(error_type error) {
     return no_err;
 }
 
+error_type put_error(error_type error, Stack* stk) {
+    if(stk == NULL)
+        return null_ptr_err;
+    stk->errors |= error;
+    return no_err;
+}
+
 error_type check_cannaries(Stack* stk) {
     if(stk->left_canary == left_parrot && stk->right_canary == right_parrot)
         return no_err;
@@ -122,5 +112,22 @@ error_type check_cannaries(Stack* stk) {
 }
 
 error_type stack_resize_capacity(Stack* stk) {
+    Elem_t* temp = NULL;
 
+    if ((stk->size == stk->capacity / 2 - 1) && (stk->capacity / 2 >= min_capacity)) {
+        temp = (Elem_t*)realloc(stk->data, stk -> capacity  / 2 * sizeof(Elem_t));
+        if(temp == NULL)
+            return mem_alloc_err;
+        stk->data = temp;
+        stk->capacity = stk->capacity /  2;
+    } else if(stk->size == stk->capacity - 1) {
+        temp = (Elem_t*)realloc(stk->data, stk->capacity * 2 * sizeof(Elem_t));
+        if(temp == NULL)
+            return mem_alloc_err;
+        stk->data = temp;
+        for(int i = 0; i < stk->size + 2; i++)
+            stk->data[stk->size  + i] = void_elem;
+        stk->capacity *= 2;
+    }
+    return no_err;
 }
--- stack.h
+++ stack.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define STK_DUMP(stk) ({                                                \
-    dump((stk), __FILE__, __PRETTY_FUNCTION__, __LINE__);               \
+    stack_dump((stk), __FILE__, __PRETTY_FUNCTION__, __LINE__);         \
     })
 
 #define STK_CTOR(stk) ({                                                \
@@ -14,7 +14,7 @@
 typedef int Elem_t;
 const unsigned long long left_parrot = 12250030;  //Does not include the digits 0 or 1 in the octal notation of the number
 const unsigned long long right_parrot = 4831533;  //Does not include the digits 0 or 1 in the octal notation of the number
-const int void_elem = 78787;
+const Elem_t void_elem = 78787;
 
 struct Stack {
     unsigned long long left_canary = left_parrot;
@@ -26,6 +26,7 @@ struct Stack {
     const char* func_name = NULL;
     const char* name = NULL;
     bool is_exist = 0;
+    int errors = 1 << 0;
     unsigned long long right_canary = right_parrot;
 };
 
@@ -35,19 +36,21 @@ enum error_type {
 
     mem_alloc_err = 1 << 1,
     null_ptr_err = 1 << 2,
-    out_of_index_err = 1212,
-    stack_is_not_exist = 148814,
-    critical_err = 6666,
+    out_of_index_err = 1 << 3,
+    stack_is_not_exist = 1 << 4,
+    critical_err = 1 << 5,
     stack_canary_err = 1 << 6
 };
 
 error_type stack_ctor(Stack* stk, const char* name, int line, const char* func, const char* file);
 error_type stack_pop(Stack* stk, Elem_t* poped);
 error_type stack_dtor(Stack* stk);
-error_type dump(Stack* stk, const char* file, const char* func, const int line);
+error_type stack_dump(Stack* stk, const char* file, const char* func, const int line);
 error_type get_error(error_type error);
 error_type stack_push(Stack* stk, Elem_t value);
 error_type check_cannaries(Stack* stk);
+error_type stack_resize_capacity(Stack* stk);
+error_type put_error(error_type error, Stack* stk);
 
 
 #endif // STACK_H_INCLUDED
